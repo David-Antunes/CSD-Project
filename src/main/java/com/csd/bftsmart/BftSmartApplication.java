9f35e0b979130212e2695745c1e676a317a9b0ff
David Antunes <dmm.antunes@campus.fct.unl.pt>
2022-03-31 15:06:39 +0100

    Added Initial Files

---
+++ src/main/java/com/csd/bftsmart/BftSmartApplication.java
@@ -0,0 +1,13 @@
+package com.csd.bftsmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BftSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BftSmartApplication.class, args);
+	}
+
+}
