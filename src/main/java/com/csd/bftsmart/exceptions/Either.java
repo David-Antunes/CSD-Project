535d26100a24252a394c8539b21967cfa31ee771
André Palma <andremarquespalma@gmail.com>
2022-05-28 01:19:57 +0100

    Fix Either<> Equals and HashCode

--- src/main/java/com/csd/bftsmart/exceptions/Either.java
+++ src/main/java/com/csd/bftsmart/exceptions/Either.java
@@ -1,7 +1,10 @@
 package com.csd.bftsmart.exceptions;
 
+import lombok.EqualsAndHashCode;
+
 import java.io.Serializable;
 
+@EqualsAndHashCode
 public class Either<T> implements Serializable {
     private final ExceptionCode code;
     private final T result;
@@ -22,9 +25,11 @@ public class Either<T> implements Serializable {
     public static <T> Either<T> success(T t) {
         return new Either<>(ExceptionCode.SUCCESS, t);
     }
+
     public static <T> Either<T> success() {
         return new Either<>(ExceptionCode.SUCCESS, null);
     }
+
     public static <T> Either<T> failure(ExceptionCode t) {
         return new Either<>(t, null);
     }
